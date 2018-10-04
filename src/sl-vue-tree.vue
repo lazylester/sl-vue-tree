0d526ddd3d050ca9cd3f5c702a0751fc6e713620
lazylester <codehacker@comcast.net>
2018-10-04 16:44:26 -0700

    make empty-node slot available in nested folders

--- src/sl-vue-tree.vue
+++ src/sl-vue-tree.vue
@@ -109,6 +109,12 @@
           <template slot="sidebar" slot-scope="{ node }">
             <slot name="sidebar" :node="node"></slot>
           </template>
+          
+          <template slot="empty-node" slot-scope="{ node }">
+            <slot name="empty-node" :node="node" v-if="!node.isLeaf && node.children.length == 0 && node.isExpanded">
+              <div> empty </div>
+            </slot>
+          </template>
 
         </sl-vue-tree>
 
